1419282eb995b3efc682ef11563b904542f8bf0b
Alan Henry <alandavidhenry@gmail.com>
2022-02-07 22:58:06 +0000

    Move jQuery from javascript.js to home.ejs

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,4 +1,4 @@
-// https://formden.com/blog/date-picker
+https://formden.com/blog/date-picker
 $(document).ready(function(){
     var date_input=$('input[name="date"]'); //our date input has the name "date"
     var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
@@ -9,11 +9,3 @@ $(document).ready(function(){
         autoclose: true,
     })
 })
-
-$("input[name='radioGuests']").change(function() {
-    $("#collapseGuests").toggle();
-});
-
-$("input[name='radioPOCDetails']").change(function() {
-    $("#collapsePOCDetails").toggle();
-});
